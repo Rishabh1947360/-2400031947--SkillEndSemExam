0adaa332689b7ac242aa61c1ad765ad2bbe6a399
Rishabh1947360 <rishabhkumar009990@gmail.com>
2025-11-28 17:16:11 +0530

    Add files via upload

---
+++ App.jsx
@@ -0,0 +1,17 @@
+
+
+import LoginForm from "./LoginForm";
+import RegisterForm from "./RegisterForm";
+
+function App() {
+  return (
+    <div style={{ display: "flex" }}>
+      <LoginForm />
+      <RegisterForm />
+    </div>
+  );
+}
+
+export default App;
+
+
